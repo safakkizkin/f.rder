fc7766da00b6548467f36806330d33282a854614
safakkizkin <safakkizkin@gmail.com>
2020-08-03 22:27:00 +0300

    added commandline args

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 )
 
 var _currentDirectory = ""
+var _workingDirectory = ""
 
 func main() {
+	argsFlagParser()
+	checkFiles()
+
 	fileName := "0C685917-3058-4E01-8317-A4D78FB189A2.jpg"
 	fileStat, err := os.Stat(fileName)
 	if err != nil {
@@ -20,6 +26,22 @@ func main() {
 	createDirectory(strconv.Itoa(fileStat.ModTime().Year()))
 	createDirectory(fileStat.ModTime().Month().String())
 	os.Rename(fileName, path.Join(_currentDirectory, fileName))
+}
+
+func checkFiles() {
+	var files []string
+	err := filepath.Walk(_workingDirectory, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file)
+	}
 }
 
 func createDirectory(folderName string) {
@@ -32,4 +54,8 @@ func createDirectory(folderName string) {
 			log.Fatal(err)
 		}
 	}
+}
+
+func argsFlagParser() {
+	_workingDirectory := flag.String("folderPath", "", "path of the place that photos includes")
 }
